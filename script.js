109bbb9e9147aed1e9d6d5b2d2ab385fa545b185
EkoueJojo <92011395+EkoueJojo@users.noreply.github.com>
2024-06-26 15:53:37 +0200

    clearInterval

--- script.js
+++ script.js
@@ -180,11 +180,13 @@ function eteindreBougie()
 
 function showLettre()
 {
+  clearInterval(smokeGenerationInterval);
+  clearInterval(smokeAnimationInterval);
+
   lettreContainer.classList.add("fadein_animation");
   lettreContainer.style.display = "initial";
   flecheBtn.classList.remove("fadein_animation");
   flecheBtn.classList.add("fadeout_animation");
-  clearInterval(smokeGenerationInterval);
   flecheBtn.removeEventListener("click", showLettre);
   typeLettre();
 }
